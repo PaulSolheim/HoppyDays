63805ec74f931b3d9e1d55026cd0cc2e572a3e18
Pål Grønstad Solheim <paul.solheim@gmail.com>
2018-09-10 01:51:38 +0200

    Til 3.gang
    Klargjort til 3. kodekveld
    Flyttet animasjoner til nytt skript
    Lagt til rette i Player skriptet
    Laget ny scene Platforms,
    plattformer med kollisjons-soner

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,15 +1,37 @@
 extends KinematicBody2D
 
 const SPEED = 750
+const GRAVITY = 3600
+const JUMP_SPEED = -1800
+const UP = Vector2(0,-1)
 
 var motion = Vector2()
 
 func _physics_process(delta):
-	if Input.is_action_pressed("ui_right"):
+	update_motion(delta)
+
+func update_motion(delta):
+	fall(delta)
+	run()
+	jump()
+	move_and_slide(motion, UP)
+
+func _process(delta):
+	update_animation(motion)
+
+func update_animation(motion):
+	$AnimatedSprite.update(motion)
+
+func fall(delta):
+	pass
+
+func run():
+	if Input.is_action_pressed("ui_right") and not Input.is_action_pressed("ui_left"):
 		motion.x = SPEED
-	elif Input.is_action_pressed("ui_left"):
+	elif Input.is_action_pressed("ui_left") and not Input.is_action_pressed("ui_right"):
 		motion.x = -SPEED
 	else:
 		motion.x = 0
-	move_and_slide(motion)
-	
+
+func jump():
+	pass
