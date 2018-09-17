d99f4131f7dce69f1d101d797df35127b050ed06
Pål Grønstad Solheim <paul.solheim@gmail.com>
2018-09-17 15:44:29 +0200

    Til 4. Gang
    Klargjort til 4. kodekveld. Lagt inn ParallaxBackground, SpikesTop og SpikesBottom.

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -23,7 +23,7 @@ func update_animation(motion):
 	$AnimatedSprite.update(motion)
 
 func fall(delta):
-	if is_on_floor():
+	if is_on_floor() or is_on_ceiling():
 		motion.y = 0
 	else:
 		motion.y += GRAVITY * delta
---
+++ Scripts/SpikesBottom.gd
@@ -0,0 +1,4 @@
+extends Area2D
+
+func _on_SpikesBottom_body_entered(body):
+	print("Ouch!")
