4de102a82dc864deb2262d541c266cbb9598f5b1
Pål Grønstad Solheim <paul.solheim@gmail.com>
2018-10-17 02:38:06 +0200

    Cloud og SpaceShip
    Lagt til Cloud og SpaceShip inkl. Lightning og Missile. Klar for siste Kodekveld med HoppyDays.

--- Scripts/Global.gd
+++ Scripts/Global.gd
@@ -7,7 +7,8 @@ var GUI
 # Scene locations - MUST be set MANUALLY
 var GameOver = "res://Scenes/Levels/GameOver.tscn"
 var Victory = "res://Scenes/Levels/Victory.tscn"
-var Lightning = ""
+var Lightning = "res://Scenes/Lightning.tscn"
+var Missile = "res://Scenes/Missile.tscn"
 var Levels = []
 
 func _ready():
---
+++ Scripts/Missile.gd
@@ -0,0 +1,23 @@
+extends Area2D
+
+const SPEED = 500
+
+func _ready():
+	set_as_toplevel(true)
+
+func _physics_process(delta):
+	move_down_screen(delta)
+	manage_collision()
+	
+	if not $VisibilityNotifier2D.is_on_screen():
+		queue_free()
+
+func move_down_screen(delta):
+	global_position.y += SPEED * delta
+
+func manage_collision():
+	var collider = get_overlapping_bodies()
+	for object in collider:
+		if object == Global.Player:
+			Global.GameState.hurt()
+		queue_free()
